92b7cbfa7bc66ec29af4b1b434a93bc1744bb0a2
CloudSec9-ca <github@cloudsec9.ca>
2021-03-26 17:21:23 -0400

    Add the header which allows basic access to the main registry header

---
+++ include/registry.h
@@ -0,0 +1,35 @@
+
+/*
+	Start with some basic registry bits
+*/
+#define HIVE_FILENAME_MAXLEN		31
+typedef ULONG HCELL_INDEX, *PHCELL_INDEX;
+typedef struct _HBASE_BLOCK
+{
+	ULONG Signature;
+
+	ULONG Sequence1;
+	ULONG Sequence2;
+
+	LARGE_INTEGER TimeStamp;
+
+	ULONG Major;
+	ULONG Minor;
+
+	ULONG Type;
+	ULONG Format;
+
+	HCELL_INDEX RootCell;
+	ULONG Length;
+	ULONG Cluster;
+
+	WCHAR FileName[HIVE_FILENAME_MAXLEN + 1];
+	ULONG Reserved1[99];
+
+	ULONG CheckSum;
+
+	ULONG Reserved2[0x37E];
+	//ULONG Reserved2[0x37C];
+	ULONG BootType;
+	ULONG BootRecover;
+} HBASE_BLOCK, *PHBASE_BLOCK;
